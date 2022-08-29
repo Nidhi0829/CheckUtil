8e267aa50d4d864fb45245f33a32a150f791f3c7
Nidhi <nidhi.sharma0829@outlook.com>
2022-08-29 23:09:50 +0530

    Python apm

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ import setuptools
 setuptools.setup(
     name="CheckUtil",
     version="0.4",
-    author="Nidhi Sharma",
+    author="Nidhi_Sharma",
     description="This package is use to check the RAM and CPU Usage of Current Device.",
+    long_description=" ",
     packages=["CheckUtil"],
-    url = "https://github.com/Nidhi0829/CheckUtil"
+    url = "https://github.com/Nidhi0829/CheckUtil",
 )
